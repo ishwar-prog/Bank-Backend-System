cc62d90a34764cf5ec8f0b09d0661a2315108e29
Ishwar Suthar <ishwar16suthar@gmail.com>
2026-03-22 03:25:32 +0530

    Add authentication features with user registration and login
    - Implement user registration with email, name, and password
    - Add JWT token generation and cookie handling for authentication
    - Create user login functionality with password validation
    - Update dependencies in package.json and package-lock.json
    - Integrate cookie-parser middleware in app.js

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,75 @@
 const userModel = require("../models/user.model")
+const jwt = require("jsonwebtoken")
 
-function userRegisterController(req, res){
-    
+async function userRegisterController(req, res){
+
+    const {email, name, password} = req.body;
+
+    const isExists = await userModel.findOne({email:email});
+
+    if(isExists){
+        return res.status(422).json({
+            message: "User already exists",
+            status: "FAILED"
+        });
+    }
+
+    const user = await userModel.create({
+        email, password , name
+    });
+
+    const token = jwt.sign({userId:user._id}, process.env.JWT_SECRET_KEY, {expiresIn:"3d"});
+
+    res.cookie("token", token, {httpOnly: true, secure: true});
+
+    res.status(201).json({
+        user:{
+            _id:user._id,
+            email:user.email,
+            name:user.name
+        },
+        token
+    });
+
+}
+
+async function userLoginController(req, res){
+
+    const {email, password} = req.body;
+
+    const user = await userModel.findOne({email}).select("+password");
+
+    if(!user){
+        return res.status(401).json({
+            message: "Invalid email or password",
+            
+        })
+    }
+
+    const isValidPassword = await user.comparePassword(password);
+
+    if(!isValidPassword){
+        return res.status(401).json({
+            message: "Invalid email or password",
+        })
+    }
+
+    const token = jwt.sign({userId:user._id}, process.env.JWT_SECRET_KEY, {expiresIn:"3d"});
+
+    res.cookie("token", token, {httpOnly: true, secure: true});
+
+       res.status(200).json({
+        user:{
+            _id:user._id,
+            email:user.email,
+            name:user.name
+        },
+        token
+    });
+
+}
+
+module.exports = {
+    userRegisterController,
+    userLoginController
 }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const authController = require("../controllers/auth.controller")
 
 const router = express.Router();
 
-router.post("/register")
+router.post("/register", authController.userRegisterController)
+
+router.post("/login",authController.userLoginController)
 
 module.exports = router;

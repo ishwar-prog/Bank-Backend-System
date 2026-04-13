2938d5c92e10f866d6e22e918706f13055759a60
Ishwar Suthar <ishwar16suthar@gmail.com>
2026-04-13 23:37:59 +0530

    Fix code formatting in transaction and user models for improved consistency and readability

--- src/models/transaction.model.js
+++ src/models/transaction.model.js
@@ -2,39 +2,39 @@ const mongoose = require("mongoose")
 
 const transactionSchema = new mongoose.Schema({
 
-    fromAccount:{
+    fromAccount: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Account",
-        required:[true, "From Account is required"],
+        required: [true, "From Account is required"],
         index: true
     },
-    toAccount:{
+    toAccount: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Account",
-        required:[true, "To Account is required"],
+        required: [true, "To Account is required"],
         index: true
 
     },
-    status:{
+    status: {
         type: String,
-        enum:{
-            values: ["PENDING", "COMPLETED", "FAILED","REVERSED"],
+        enum: {
+            values: ["PENDING", "COMPLETED", "FAILED", "REVERSED"],
             message: "Status must be either PENDING, COMPLETED, FAILED or REVERSED",
         },
         default: "PENDING"
     },
-    amount:{
+    amount: {
         type: Number,
         required: [true, "Amount is required"],
         min: [0, "Amount must be greater than 0"]
     },
-    idempotencyKey:{
+    idempotencyKey: {
         type: String,
         required: [true, "Idempotency Key is required"],
         unique: true,
         index: true
     }
-},{
+}, {
     timestamps: true
 })
 
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,36 +2,36 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-    email:{
-        type:String,
-        required:[true,"Email is required for creating a user"],
+    email: {
+        type: String,
+        required: [true, "Email is required for creating a user"],
         trim: true,
         lowercase: true,
-        match:[/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "Invalid email address"],
-        unique:[true,"Email already exists"]
+        match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "Invalid email address"],
+        unique: [true, "Email already exists"]
     },
-    name:{
-        type:String,
-        required:[true,"Name is required for creating an account"],
+    name: {
+        type: String,
+        required: [true, "Name is required for creating an account"],
     },
-    password:{
-        type:String,
-        required:[true,"Password is required for creating an account"],
-        minlength:[6,"Password must be at least 6 characters long"],
-        select:false
+    password: {
+        type: String,
+        required: [true, "Password is required for creating an account"],
+        minlength: [6, "Password must be at least 6 characters long"],
+        select: false
     },
-    systemUser:{
+    systemUser: {
         type: Boolean,
         default: false,
         immutable: true,
         select: false
     }
-},{
-    timestamps:true
+}, {
+    timestamps: true
 })
 
-userSchema.pre('save', async function(next){
-    if(!this.isModified('password')){
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
         return
     }
     const hash = await bcrypt.hash(this.password, 10);
@@ -39,7 +39,7 @@ userSchema.pre('save', async function(next){
     return
 })
 
-userSchema.methods.comparePassword = async function(password){
+userSchema.methods.comparePassword = async function (password) {
     return await bcrypt.compare(password, this.password);
 }
 
